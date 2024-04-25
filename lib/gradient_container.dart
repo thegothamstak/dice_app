8e38e6081a007f8dc9c653a1950b9022ec79a0dc
Shripad Tak <shripad121196@gmail.com>
2024-04-25 16:32:30 -0400

    initial push of random dice rolling app in flutter

---
+++ lib/gradient_container.dart
@@ -0,0 +1,27 @@
+import 'package:dice_app/dice_roller.dart';
+import 'package:flutter/material.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key, required this.colors});
+
+  final List<Color> colors;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: colors,
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
